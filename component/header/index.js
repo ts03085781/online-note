610a81a212381ad905248714e38c6638851a7a11
LaiYiLin <ts03085781@gmail.com>
2021-10-25 00:59:33 +0800

    gitflow-feature-stash: 建構基礎架構

---
+++ component/header/index.js
@@ -0,0 +1,11 @@
+import styles from "./header.module.scss"
+
+const  Header = () => {
+    return (
+        <div className={styles.container}>
+            12312345
+        </div>
+    );
+}
+
+export default Header
